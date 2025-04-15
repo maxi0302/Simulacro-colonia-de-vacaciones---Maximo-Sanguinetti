f841fcb3dd5bbd0a09335281802cefb123b5d0ff
maxi0302 <111788863+maxi0302@users.noreply.github.com>
2025-04-15 11:32:10 -0300

    Empty y deteccion de errores

---
+++ Assets/ColoniaDeVacaciones.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoniaDeVacaciones : MonoBehaviour
+{
+    public int infantiles;
+    public int juveniles;
+    int profInfantiles;
+    int profJuveniles;
+    int cordinadores;
+
+    void Start()
+    {
+        if (infantiles < 0 || juveniles < 0)
+        {
+            Debug.Log("ERROR: Cantidad de alumnos no valida");
+            return;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
